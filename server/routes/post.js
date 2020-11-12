6a6277d0525ec8828cace0c17b39c3af30f02fc4
Swetha41 <swethagottimukkala@gmail.com>
2020-11-12 13:55:30 +0530

    comments added

--- server/routes/post.js
+++ server/routes/post.js
@@ -8,6 +8,7 @@ const requireLogin = require('../middleware/requireLogin');
 router.get('/allpost', requireLogin, (req, res) => {
     Post.find()
     .populate("postedBy", "_id name")
+    .populate("comments.postedBy","_id name")
     .then(posts => {
         res.json({posts})
     })
@@ -80,6 +81,43 @@ router.put('/unlike', requireLogin, (req,res) => {
         }
         else{
             res.json(result)
+        }
+    })
+})
+
+//comment
+router.put('/comment', requireLogin, (req,res) => {
+    const comment = {
+        text : req.body.text,
+        postedBy: req.user._id
+    }
+    Post.findByIdAndUpdate(req.body.postId, {
+        $push:{comments:comment}
+    },{
+        new:true
+    })
+    .populate("comments.postedBy", "_id name")
+    .populate("postedBy", "_id name")
+    .exec((err, result) => {
+        if(err){
+            return res.status(422).json({error:err})
+        }
+        else{
+            res.json(result)
+        }
+    })
+})
+
+//delete post
+router.delete('/deletepost/:postId', requireLogin, (req,res) => {
+    Post.findOne({_id:req.params.postId})
+    .populate("postedBy", "_id")
+    .exec((err,post) => {
+        if(err || !post){
+            return req.status(422).json({error:err})
+        }
+        if(post.postedBy._id.toString() === req.user._id.toString()){
+
         }
     })
 })
