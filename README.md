ce69f22c371a17dbcd252e60960571d65fee7a20
Swetha41 <swethagottimukkala@gmail.com>
2020-11-15 15:31:53 +0530

    deployed to heroku

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Instagram_Clone_MERN
 Instagram application by MERN stack
+
+Run commands:
+
+1. Clone the Repo 
+2. Run "npm install" in both server and client folder
+3. After finishing installations run "npm start"  
+4. App will run on mentioned port
+
+
+Live Demo:
+https://instaclone41.herokuapp.com
+
--- server/app.js
+++ server/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
+const app = express();
 const mongoose = require('mongoose');
 require('dotenv').config();
-
-const app = express();
+const PORT = process.env.PORT || 5000;
 
 //DB connection
 mongoose.connect(process.env.MONGO_URI, {
@@ -15,13 +15,21 @@ mongoose.connect(process.env.MONGO_URI, {
 
 require('./models/user');
 require('./models/post');
+
 app.use(express.json());
 app.use(require('./routes/auth'));
 app.use(require('./routes/post'));
 app.use(require('./routes/user'));
 
+if(process.env.NODE_ENV=="production"){
+    app.use(express.static('client/build'))
+    const path = require('path')
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
+
 //Port
-const PORT = 5000;
 app.listen(PORT, (req, res) => {
     console.log(`server started at port ${PORT}`)
 });
